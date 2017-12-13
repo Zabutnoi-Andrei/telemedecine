63bedcf0c9851deab960cc24aae5fd89ae203c57
KseniaZv <xeniazvercova@gmail.com>
2017-12-14 00:09:44 +0200

    Splash Screen, Welcome Screen

---
+++ Telemedicine/app/src/main/java/com/example/zveri/telemedicine/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zveri.telemedicine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+}
