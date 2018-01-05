cb586f2c8a7112d9b9e42a19850c6ef018541c53
RiDEN <dredo.97@gmail.com>
2018-01-05 14:24:36 +0200

    DoctorDetails view done.

---
+++ app/src/main/java/com/example/zveri/telemedicine/DoctorDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.zveri.telemedicine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zveri.telemedicine.R;
+
+public class DoctorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_details);
+    }
+}
